eecc38f52255c17c084219cef9f980c711aa79b0
AinaEmmanuel <ainae06@gmail.com>
2021-03-16 22:31:51 +0100

    Get all parameters

--- CRD.py
+++ CRD.py
@@ -17,3 +17,23 @@ def crd_parameters(data):
     # Calculate the Correction Factor
     GT = sum(total_each_trt)
     CF = round(GT**2/N, 1)
+
+    # Calculate the Sum of Square Treatment
+    square_tot_trt = [val**2 for val in total_each_trt]
+    sum_square_tot_trt = sum(square_tot_trt)
+    SStrt = round((sum_square_tot_trt/k) - CF, 1)
+    
+    # Calculate the Sum of Square Total
+    all_units = []
+    for col in data.columns:
+        for val in data[col]:
+            all_units.append(val)
+    square_all_units = [val**2 for val in all_units]
+    SStot = round(sum(square_all_units) - CF, 1)
+
+    # Calculate the Sum of Square Error
+    SSerr = round(SStot -SStrt, 1)
+
+    return(SStrt, SSerr, SStot)
+
+print(crd_parameters(data))

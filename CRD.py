6a6a2b48cd11375fa430464f12ed863cd7a872d6
AinaEmmanuel <ainae06@gmail.com>
2021-03-16 22:19:40 +0100

    calc total and CF

--- CRD.py
+++ CRD.py
@@ -7,3 +7,13 @@ data = pd.read_csv('/home/emmanuel/Emmanuel/Work/Experiment Design/CRD.csv')
 # Base Variables
 N = data.shape[0] * data.shape[1]
 k = data.shape[0]
+
+def crd_parameters(data):
+    # Calculate Grand Total
+    total_each_trt = []
+    for col in data.columns:
+        total_each_trt.append(data[col].sum())
+        
+    # Calculate the Correction Factor
+    GT = sum(total_each_trt)
+    CF = round(GT**2/N, 1)
